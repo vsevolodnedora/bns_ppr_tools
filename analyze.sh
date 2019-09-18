b66b58d758710f6faccd84affde2235b1cd07efa
vnedora <vsevolod.nedora@uni-jena.de>
2019-09-18 22:29:47 +0200

    1. created analyze.sh for a pipeline analysis

--- analyze.sh
+++ analyze.sh
@@ -16,13 +16,13 @@ target=$2
 output=$3
 
 # creates ittime.h5 file that maps iterations and timesteps of available data
-#call python preanalysis.py -s $sim -i $2 -o $3 -t update_status || exit 1
+call python preanalysis.py -s $sim -i $2 -o $3 -t update_status || exit 1
 
 # shows the output directories times and number of files avialable
-#call python preanalysis.py -s $sim -i $2 -o $3 -t print_status || exit 1
+call python preanalysis.py -s $sim -i $2 -o $3 -t print_status || exit 1
 
 # creates .h5 file with remaped outflow data for a spherical grid
-#call python outflowed.py -s $sim -i $2 -o $3 -t reshape -d 0 || exit 1
+call python outflowed.py -s $sim -i $2 -o $3 -t reshape -d 0 || exit 1
 
 # do outflwoed analysis for detector -d 0  and for mask -m  geo (geodesic)
 call python outflowed.py -s $sim -i $2 -o $3 -t all -m geo -d 0 --overwrite yes || exit 1
